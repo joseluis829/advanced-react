dca9cd9c5b3cb8dc2314da5f54811d7572cc1eb2
Jose Luis <jose.cullcay@ucuenca.edu.ec>
2018-05-29 00:52:56 -0500

    Switch and Toggle with styles

---
+++ src/components/Toggle.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Switch from "./Switch";
+
+class Toggle extends Component {
+    
+    state = { on: false }
+
+    toggle = () => this.setState(({on}) => ({on: !on}), () => {
+        this.props.onToggle(this.state.on)
+    })
+
+    render() {
+        const {on} = this.state
+        return (
+            <Switch on={on} onClick = {this.toggle} />
+        );
+    }
+}
+
+export default Toggle;
