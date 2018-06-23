35a3e6d83daf9c1dfc30e98408a34c5957534b31
Jose Luis Cullcay <jcullcay@tangocode.com>
2018-06-22 21:21:19 -0500

    Seven lesson

---
+++ src/components/07.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Switch } from './switch'
+
+class Toggle extends React.Component {
+
+    state = { on: false }
+
+    toggle = () => {
+        this.setState(
+            ({ on }) => ({ on: !on }),
+            () => {
+                this.props.onToggle(this.state.on)
+            },
+        )
+    }
+
+    render() {
+        return this.props.children({
+            on: this.state.on,
+            toggle: this.toggle,
+        })
+    }
+}
+
+function CommonToggle(props) {
+    return (
+        <Toggle {...props} >
+            {({on, toggle}) => <Switch on={on} onClick={toggle} />}
+        </Toggle>
+    )
+}
+
+function Usage({
+    onToggle = (...args) => console.log('onToggle', ...args),
+}) {
+    return <CommonToggle onToggle={onToggle} />
+    /*return (
+        <Toggle onToggle={onToggle}>
+            {({ on, toggle }) => (
+                <div>
+                    {on ? 'The button is on' : 'The button is off'}
+                    <Switch on={on} onClick={toggle} />
+                    <hr />
+                    <button aria-label="custom-button" onClick={toggle}>
+                        {on ? 'on' : 'off'}
+                    </button>
+                </div>
+            )}
+        </Toggle>
+    )*/
+}
+
+export default Usage;
