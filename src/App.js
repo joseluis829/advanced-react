ffa0841434ac170cda7071232efbb60e559d725a
Jose Luis Cullcay <jcullcay@tangocode.com>
2018-07-06 07:01:08 -0500

    15

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Usage from './components/14.jsx';
+import Usage from './components/15.jsx';
 //import { Toggle } from "./components/01.jsx";
 
 class App extends Component {
